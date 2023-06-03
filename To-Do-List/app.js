395df6ce3892ed70d2f4f01f42dd70978ed2685e
HemanthDas <103018057+HemanthDas@users.noreply.github.com>
2023-06-04 01:29:17 +0530

    day-1 changes updated

--- To-Do-List/app.js
+++ To-Do-List/app.js
@@ -2,31 +2,28 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const app = express();
 const ejs = require("ejs");
+
 app.set("view engine", "ejs");
+app.use(bodyparser.urlencoded({ extended: true }));
+let items = ["good morning"];
+const option = {
+  day: "numeric",
+  month: "long",
+  weekday: "long",
+  year: "numeric",
+};
 app.get("/", function (req, res) {
   var today = new Date();
-  res.render("list.ejs", { Today: dayGiver(today.getDay()) });
+  res.render("list.ejs", {
+    Today: today.toLocaleDateString("en-us", option),
+    list: items,
+  });
 });
-
+app.post("/", (req, res) => {
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
 app.listen(3000, () => {
   console.log("app running in http://localhost:3000/");
 });
-
-const dayGiver = (day) => {
-  switch (day) {
-    case 0:
-      return "Sunday";
-    case 1:
-      return "Monday";
-    case 2:
-      return "Tuesday";
-    case 3:
-      return "Wednesday";
-    case 4:
-      return "Thursday";
-    case 5:
-      return "Friday";
-    case 6:
-      return "Saturday";
-  }
-};
