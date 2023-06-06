73684b6bb0e63be62610f736dc0ef226386b0d1e
HemanthDas <103018057+HemanthDas@users.noreply.github.com>
2023-06-06 23:05:47 +0530

    routing more

--- Dairy-Blog/app.js
+++ Dairy-Blog/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+const posts = [];
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -13,12 +15,8 @@ const aboutContent =
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-//Home
 app.get("/", (req, res) => {
-  res.render("home.ejs", { HomeContent: homeStartingContent });
-});
-app.listen(3000, () => {
-  console.log("Server running in http://localhost:3000/");
+  res.render("home.ejs", { HomeContent: homeStartingContent, posts: posts });
 });
 app.get("/blog", (req, res) => {
   res.render("contact.ejs", { ContactContent: contactContent });
@@ -30,5 +28,13 @@ app.get("/compose", (req, res) => {
   res.render("compose.ejs");
 });
 app.post("/compose", (req, res) => {
-  console.log(req.body.postTitle);
+  let post = {
+    title: req.body.title,
+    blog: req.body.post,
+  };
+  posts.push(post);
+  res.redirect("/");
+});
+app.listen(3000, () => {
+  console.log("Server running in http://localhost:3000/");
 });
